8a2ba50255029ef223c3b91d6d9b4d2fc6519be4
Vineet <100793773+Mr-Vineet@users.noreply.github.com>
2025-05-19 11:12:13 +0530

    adds more test cases for square methods

--- test/com/tw/shapes/SquareTest.java
+++ test/com/tw/shapes/SquareTest.java
@@ -6,14 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SquareTest {
     @Test
-    void CalculatesAreaOfSquare() {
+    void calculatesAreaOfSquare() {
         final Square sq = new Square(4);
         assertEquals(16, sq.area());
+    }
+
+    @Test
+    void calculatesAreaOfSquareWithSidesZero() {
+        final Square square = new Square(0);
+        assertEquals(0, square.area());
     }
 
     @Test
     void calculatesPerimeterOfSquare() {
         final Square sq = new Square(2);
         assertEquals(8, sq.perimeter());
+    }
+
+    @Test
+    void calculatesPerimeterOfSquareWithSidesZero() {
+        final Square square = new Square(0);
+        assertEquals(0, square.perimeter());
+
     }
 }
