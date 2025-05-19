9dd9060417ec676d274ce668d7607c2818788d3f
Vineet <100793773+Mr-Vineet@users.noreply.github.com>
2025-05-19 10:49:34 +0530

    calculate rectangle area

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package com.tw.dummy;
-
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello world");
---
+++ test/RectangleTest.java
@@ -0,0 +1,18 @@
+import com.tw.shapes.Rectangle;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+    @Test
+    void calculatesAreaWithSameLengthAndBreadth() {
+        final Rectangle rectangle = new Rectangle(4,4);
+        assertEquals(16, rectangle.area());
+    }
+
+    @Test
+    void calculatesAreaWithDifferentLengthAndBreadth() {
+        final Rectangle actual = new Rectangle(2, 3);
+        assertEquals(6, actual.area());
+    }
+}
